8375f18054428c79888eeb41fd3dfb5202559ecd
pkrakowski <69652286+pkrakowski@users.noreply.github.com>
2023-03-26 13:32:40 +0100

    Update Graph.SendMail.psm1

--- Graph.SendMail/Graph.SendMail.psm1
+++ Graph.SendMail/Graph.SendMail.psm1
@@ -1,65 +1,70 @@
 function Send-GraphMail {
-  [CmdletBinding()]
-  param (
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$ClientId,
 
-    [Parameter(Mandatory)]
-    [string]$clientId,
-    
-    [Parameter(Mandatory)]
-    [string]$clientSecret,
+        [Parameter(Mandatory)]
+        [string]$ClientSecret,
 
-    [Parameter(Mandatory)]
-    [string]$TenantName,
+        [Parameter(Mandatory)]
+        [string]$TenantName,
 
-    [Parameter(Mandatory)]
-    [string]$From,
+        [Parameter(Mandatory)]
+        [string]$From,
 
-    [Parameter(Mandatory)]
-    [string[]]$Recipients,
+        [Parameter(Mandatory)]
+        [string[]]$Recipients,
 
-    [Parameter(Mandatory)]
-    [string]$Subject,
+        [Parameter(Mandatory)]
+        [string]$Subject,
 
-    [Parameter(Mandatory)]
-    [string]$EmailBody
-  )
+        [Parameter(Mandatory)]
+        [string]$EmailBody
+    )
 
-$ReqTokenBody = @{
-  Grant_Type    = "client_credentials"
-  Scope         = "https://graph.microsoft.com/.default"
-  client_Id     = $clientID
-  Client_Secret = $clientSecret
-} 
+    $ReqTokenBody = @{
+        Grant_Type    = "client_credentials"
+        Scope         = "https://graph.microsoft.com/.default"
+        Client_Id     = $ClientId
+        Client_Secret = $ClientSecret
+    } 
 
-$TokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$TenantName/oauth2/v2.0/token" -Method POST -Body $ReqTokenBody
+    $TokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$TenantName/oauth2/v2.0/token" -Method POST -Body $ReqTokenBody
 
-foreach ($Recipient in $Recipients){
-  $Block = "
-{
-  `"EmailAddress`": {
-    `"Address`": `"$Recipient`"
-  }
-},"
-$RecipientBlock = $RecipientBlock + $Block
-}
+    $RecipientBlock = ""
+    foreach ($Recipient in $Recipients) {
+        $Block = @"
+        {
+            "EmailAddress": {
+                "Address": "$Recipient"
+            }
+        },
+"@
+        $RecipientBlock = $RecipientBlock + $Block
+    }
 
-$apiUrl = "https://graph.microsoft.com/v1.0/users/$From/sendMail"
-$body = @"
-{
-"Message": {
-  "Subject": "$Subject",
-  "Body": {
-    "ContentType": "HTML",
-    "Content": "$EmailBody"
-  },
-  "ToRecipients": [
-      $RecipientBlock
-  ]
-},
-"SaveToSentItems": "false",
-"isDraft": "false"
-}
+    $apiUrl = "https://graph.microsoft.com/v1.0/users/$From/sendMail"
+    $body = @"
+    {
+        "Message": {
+            "Subject": "$Subject",
+            "Body": {
+                "ContentType": "HTML",
+                "Content": "$EmailBody"
+            },
+            "ToRecipients": [
+                $RecipientBlock
+            ]
+        },
+        "SaveToSentItems": "false",
+        "isDraft": "false"
+    }
 "@
 
-$call = Invoke-RestMethod -Headers @{Authorization = "Bearer $($Tokenresponse.access_token)"} -Uri $apiUrl -Body $Body -Method Post -ContentType 'application/json'
+    try {
+        $call = Invoke-RestMethod -Headers @{Authorization = "Bearer $($TokenResponse.access_token)"} -Uri $apiUrl -Body $Body -Method Post -ContentType 'application/json'
+    } catch {
+        Write-Error "Error sending email: $_"
+    }
 }
